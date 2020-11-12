4ad7402db0de539d9371e4a00f0d825ccd4bfbaa
Jon Christie <fullstack11235@gmail.com>
2020-11-12 03:22:41 -0500

    Updates

---
+++ precache-manifest.71d5cb817b8d37fdf3b7341ca583ece1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa81bc21a0a234870b1b557a944a7bd",
+    "url": "/reviewIt/index.html"
+  },
+  {
+    "revision": "7ed3e05eee0bb852c98a",
+    "url": "/reviewIt/static/js/2.0fcb70b8.chunk.js"
+  },
+  {
+    "revision": "f2339cbfe965356130a6c28fcd81fba2",
+    "url": "/reviewIt/static/js/2.0fcb70b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "080f6d565c464f9dc9f6",
+    "url": "/reviewIt/static/js/main.9c958537.chunk.js"
+  },
+  {
+    "revision": "64a40fba5b53d34e1ad8",
+    "url": "/reviewIt/static/js/runtime-main.ff3180e5.js"
+  }
+]);
